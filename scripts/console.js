e57ea232280d33270836ae732609461b6d1b3439
Joey Murphy <jmurp7385@gmail.com>
2017-03-26 11:01:42 -0500

    button script

--- scripts/console.js
+++ scripts/console.js
@@ -47,117 +47,118 @@ $(function () {
         module_arr = data.split(',');
         module_arr = getArr(module_arr);
         console.log(module_arr);
-    });
-    generateBtns(module_arr);
-    $('body').attr('style', 'background-color:black');
-    $('h1').attr('style', 'color:white');
-    $('button').attr('style', 'margin-bottom:40px');
-    $('#btnlist1').addClass("col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 vspace");
-    $('#btnlist2').addClass("col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 vspace");
-    $('#btnlist3').addClass("col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 vspace");
-    $('select').addClass("col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4");
 
-    $('.button-checkbox').each(function () {
+        generateBtns(module_arr);
+        $('body').attr('style', 'background-color:black');
+        $('h1').attr('style', 'color:white');
+        $('button').attr('style', 'margin-bottom:40px');
+        $('#btnlist1').addClass("col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 vspace");
+        $('#btnlist2').addClass("col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 vspace");
+        $('#btnlist3').addClass("col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 vspace");
+        $('select').addClass("col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4");
 
-        // Settings
-        var $widget = $(this),
-            $button = $widget.find('button'),
-            $checkbox = $widget.find('input:checkbox'),
-            color = $button.data('color'),
-            i = 0,
-            isChecked,
-            element,
-            sel,
-            settings = {
-                on: {
-                    icon: 'glyphicon glyphicon-check'
-                },
-                off: {
-                    icon: 'glyphicon glyphicon-unchecked'
-                }
-            };
+        $('.button-checkbox').each(function () {
 
-        // Actions
-        function updateDisplay() {
-            isChecked = $checkbox.is(':checked');
-            // Set the button's state
-            $button.data('state', (isChecked) ? "on" : "off");
-
-            // Set the button's icon
-            $button.find('.state-icon')
-                .removeClass()
-                .addClass('state-icon ' + settings[$button.data('state')].icon);
-            // Update the button's color
-            if (isChecked) {
-                $button
-                    .removeClass('btn-danger')
-                    .addClass('btn-success active');
-                module_arr.forEach(function (module) {
-                    element = document.getElementById(module);
-                    if (element.classList.contains("btn-success")) { // IF the element has a class of btn - success
-                        updatedMods.push(module); //append element to updatedMods
-                        console.log(updatedMods); //print (updatedMods) to console to see what's there
-                        $.get("http://localhost:8080/MMM-HTML-Controller/show" + module);
-                        updatedMods = updatedMods.filter(function (item, index, inputArray) { //filters and updates mod to only one item per selection (ex. can't have two calendars)
-                            return inputArray.indexOf(item) == index;
-                        });
-
-                    } else {
-                        console.log("nothing to be seen here..");
+            // Settings
+            var $widget = $(this),
+                $button = $widget.find('button'),
+                $checkbox = $widget.find('input:checkbox'),
+                color = $button.data('color'),
+                i = 0,
+                isChecked,
+                element,
+                sel,
+                settings = {
+                    on: {
+                        icon: 'glyphicon glyphicon-check'
+                    },
+                    off: {
+                        icon: 'glyphicon glyphicon-unchecked'
                     }
+                };
 
-                });
-                sel = document.getElementById('upperLeft'); //send updatedMods to <select><options>
-                updatedMods.forEach(function (module) {
-                    var opt = document.createElement('option');
-                    opt.innerHTML = module;
-                    opt.value = module;
-                    sel.appendChild(opt);
-                });
-                $("option").val(function (idx, val) { //supposed to take away duplicates in the dropdown
-                    $(this).siblings("[value='" + val + "']").remove();
-                });
+            // Actions
+            function updateDisplay() {
+                isChecked = $checkbox.is(':checked');
+                // Set the button's state
+                $button.data('state', (isChecked) ? "on" : "off");
 
-            } else {
-                $button
-                    .removeClass('btn-success active ')
-                    .addClass('btn-danger');
-                module_arr.forEach(function (module) {
+                // Set the button's icon
+                $button.find('.state-icon')
+                    .removeClass()
+                    .addClass('state-icon ' + settings[$button.data('state')].icon);
+                // Update the button's color
+                if (isChecked) {
+                    $button
+                        .removeClass('btn-danger')
+                        .addClass('btn-success active');
+                    module_arr.forEach(function (module) {
+                        element = document.getElementById(module);
+                        if (element.classList.contains("btn-success")) { // IF the element has a class of btn - success
+                            updatedMods.push(module); //append element to updatedMods
+                            console.log(updatedMods); //print (updatedMods) to console to see what's there
+                            $.get("http://localhost:8080/MMM-HTML-Controller/show" + module);
+                            updatedMods = updatedMods.filter(function (item, index, inputArray) { //filters and updates mod to only one item per selection (ex. can't have two calendars)
+                                return inputArray.indexOf(item) == index;
+                            });
 
-                    element = document.getElementById(module);
-                    if (element.classList.contains("btn-danger")) {
-                        if (module.indexOf('-') == null) {
-                            $.get("http://localhost:8080/MMM-HTML-Controller/hide" + module);
                         } else {
-                            var x = module.indexOf('-');
-                            module.slice(x, x + 1);
-                            $.get("http://localhost:8080/MMM-HTML-Controller/hide" + module);
+                            console.log("nothing to be seen here..");
                         }
-                    } else {
-                        console.log("nothing to be seen here..");
-                    }
-                });
+
+                    });
+                    sel = document.getElementById('upperLeft'); //send updatedMods to <select><options>
+                    updatedMods.forEach(function (module) {
+                        var opt = document.createElement('option');
+                        opt.innerHTML = module;
+                        opt.value = module;
+                        sel.appendChild(opt);
+                    });
+                    $("option").val(function (idx, val) { //supposed to take away duplicates in the dropdown
+                        $(this).siblings("[value='" + val + "']").remove();
+                    });
+
+                } else {
+                    $button
+                        .removeClass('btn-success active ')
+                        .addClass('btn-danger');
+                    module_arr.forEach(function (module) {
+
+                        element = document.getElementById(module);
+                        if (element.classList.contains("btn-danger")) {
+                            if (module.indexOf('-') == null) {
+                                $.get("http://localhost:8080/MMM-HTML-Controller/hide" + module);
+                            } else {
+                                var x = module.indexOf('-');
+                                module.slice(x, x + 1);
+                                $.get("http://localhost:8080/MMM-HTML-Controller/hide" + module);
+                            }
+                        } else {
+                            console.log("nothing to be seen here..");
+                        }
+                    });
+
+                }
 
             }
-
-        }
-        // Event Handlers
-        $button.on('click', function () {
-            $checkbox.prop('checked', !$checkbox.is(':checked'));
-            $checkbox.triggerHandler('change');
-            updateDisplay();
-        });
-        $checkbox.on('change', function () {
-            updateDisplay();
-        });
-        // Initialization
-        function init() {
-            updateDisplay();
-            // Inject the icon if applicable
-            if ($button.find('.state-icon').length === 0) {
-                $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i> ');
+            // Event Handlers
+            $button.on('click', function () {
+                $checkbox.prop('checked', !$checkbox.is(':checked'));
+                $checkbox.triggerHandler('change');
+                updateDisplay();
+            });
+            $checkbox.on('change', function () {
+                updateDisplay();
+            });
+            // Initialization
+            function init() {
+                updateDisplay();
+                // Inject the icon if applicable
+                if ($button.find('.state-icon').length === 0) {
+                    $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i> ');
+                }
             }
-        }
-        init();
+            init();
+        });
     });
 });
